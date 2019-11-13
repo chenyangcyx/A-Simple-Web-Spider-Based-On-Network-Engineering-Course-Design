9de08726f67f48f4b4d3efda076382979caa57af
陈扬cyX <chenyangcyx@outlook.com>
2019-11-14 01:19:04 +0800

    继续完善了一些功能 时间： 2019年11月14日01:18:58

--- src/FileOperation/WriteDetailInfoToFile.java
+++ src/FileOperation/WriteDetailInfoToFile.java
@@ -38,27 +38,27 @@ public class WriteDetailInfoToFile {
             DownloadFile df=new DownloadFile();
 
             bw.write("标题 : "+fdd.getTitle()+System.getProperty("line.separator"));
-            bw.write("标题 : "+fdd.getTitle()+System.getProperty("line.separator"));
-            bw.write("标题 : "+fdd.getTitle()+System.getProperty("line.separator"));
-            bw.write("标题 : "+fdd.getTitle()+System.getProperty("line.separator"));
-            bw.write("标题 : "+fdd.getTitle()+System.getProperty("line.separator"));
-            bw.write("标题 : "+fdd.getTitle()+System.getProperty("line.separator"));
-            bw.write("标题 : "+fdd.getTitle()+System.getProperty("line.separator"));
-            bw.write("标题 : "+fdd.getTitle()+System.getProperty("line.separator"));
-            bw.write("标题 : "+fdd.getTitle()+System.getProperty("line.separator"));
-            bw.write("标题 : "+fdd.getTitle()+System.getProperty("line.separator"));
-            bw.write("标题 : "+fdd.getTitle()+System.getProperty("line.separator"));
-            bw.write("标题 : "+fdd.getTitle()+System.getProperty("line.separator"));
-            bw.write("标题 : "+fdd.getTitle()+System.getProperty("line.separator"));
-            bw.write("标题 : "+fdd.getTitle()+System.getProperty("line.separator"));
-            bw.write("标题 : "+fdd.getTitle()+System.getProperty("line.separator"));
-            bw.write("标题 : "+fdd.getTitle()+System.getProperty("line.separator"));
-            bw.write("标题 : "+fdd.getTitle()+System.getProperty("line.separator"));
-            bw.write("标题 : "+fdd.getTitle()+System.getProperty("line.separator"));
-            bw.write("标题 : "+fdd.getTitle()+System.getProperty("line.separator"));
-            bw.write("标题 : "+fdd.getTitle()+System.getProperty("line.separator"));
-            bw.write("标题 : "+fdd.getTitle()+System.getProperty("line.separator"));
-            bw.write("标题 : "+fdd.getTitle()+System.getProperty("line.separator"));
+            bw.write("发布时间 : "+fdd.getTitle()+System.getProperty("line.separator"));
+            bw.write("导演 : "+fdd.getTitle()+System.getProperty("line.separator"));
+            bw.write("编剧 : "+fdd.getTitle()+System.getProperty("line.separator"));
+            bw.write("主演 : "+fdd.getTitle()+System.getProperty("line.separator"));
+            bw.write("类型 : "+fdd.getTitle()+System.getProperty("line.separator"));
+            bw.write("制片国家/地区 : "+fdd.getTitle()+System.getProperty("line.separator"));
+            bw.write("语言 : "+fdd.getTitle()+System.getProperty("line.separator"));
+            bw.write("上映日期 : "+fdd.getTitle()+System.getProperty("line.separator"));
+            bw.write("片长 : "+fdd.getTitle()+System.getProperty("line.separator"));
+            bw.write("又名 : "+fdd.getTitle()+System.getProperty("line.separator"));
+            bw.write("IMDb链接 : "+fdd.getTitle()+System.getProperty("line.separator"));
+            bw.write("豆瓣评分 : "+fdd.getTitle()+System.getProperty("line.separator"));
+            bw.write("评价次数 : "+fdd.getTitle()+System.getProperty("line.separator"));
+            bw.write("5星评价所占比例 : "+fdd.getTitle()+System.getProperty("line.separator"));
+            bw.write("4星评价所占比例 : "+fdd.getTitle()+System.getProperty("line.separator"));
+            bw.write("3星评价所占比例 : "+fdd.getTitle()+System.getProperty("line.separator"));
+            bw.write("2星评价所占比例 : "+fdd.getTitle()+System.getProperty("line.separator"));
+            bw.write("1星评价所占比例 : "+fdd.getTitle()+System.getProperty("line.separator"));
+            bw.write("剧情简介 : "+fdd.getTitle()+System.getProperty("line.separator"));
+            bw.write("图片链接 : "+fdd.getTitle()+System.getProperty("line.separator"));
+            bw.write("网址 : "+fdd.getTitle()+System.getProperty("line.separator"));
         } catch (Exception e) {
             e.printStackTrace();
         }
